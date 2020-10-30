b9e15b58921cb7c92fec909b4c43f45ea7b81093
Richard McDaniel <richard@spurjobs.com>
2020-10-30 07:00:06 -0500

    Update Dockerfile

--- kafka/start-kafka.sh
+++ kafka/start-kafka.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# set env
-export BROKER_ID=1
-export KAFKA_SERVICE_PUBLIC_HOST=
-export KAFKA_SERVICE_PUBLIC_PORT=
-export ZOOKEEPER_HOST=
-export ZOOKEEPER_PORT=2181
-# run kafka
-#/opt/kafka_2.13-2.5.1/bin/kafka-server-start.sh /opt/kafka_2.13-2.5.1/config/server.properties --override broker.id=${BROKER_ID} --override listeners=PLAINTEXT://0.0.0.0:9092 --override advertised.listeners=PLAINTEXT://:9092 --override zookeeper.connect=${ZOOKEEPER_HOST}:${ZOOKEEPER_PORT} --override controlled.shutdown.enable=true
-tail -f /dev/null
+/opt/kafka_2.13-2.5.1/bin/kafka-server-start.sh /opt/kafka_2.13-2.5.1/config/server.properties --override broker.id=${HOSTNAME##*-} --override listeners=PLAINTEXT://0.0.0.0:${KAFKA_SERVICE_PORT} --override advertised.listeners=PLAINTEXT://${KAFKA_SERVICE_HOST}:${KAFKA_SERVICE_PORT} --override zookeeper.connect=${ZOOKEEPER_CLIENT_SERVICE_HOST}:${ZOOKEEPER_CLIENT_SERVICE_PORT} --override controlled.shutdown.enable=true --override log.dirs=/var/lib/kafka/data/topics
+#tail -f /dev/null
