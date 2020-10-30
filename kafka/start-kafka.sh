dee7a92032d2aca90aff03d5b6af16c017cac73a
Richard McDaniel <richard@spurjobs.com>
2020-10-30 14:48:56 -0500

    Update Dockerfile

--- kafka/start-kafka.sh
+++ kafka/start-kafka.sh
@@ -10,12 +10,12 @@ export BROKER_SERVICE_PORT_INTERNAL_EXTERNAL=${!BROKER_SERVICE_PORT_INTERNAL_EXT
 export BROKER_SERVICE_HOST_EXTERNAL=${!BROKER_SERVICE_HOST_EXTERNAL_ENV}
 export BROKER_SERVICE_PORT_EXTERNAL=${!BROKER_SERVICE_PORT_EXTERNAL_ENV}
 [[ -z "$BROKER_SERVICE_HOST_EXTERNAL" ]] && { echo "No external IP available" ; exit 1; }
-/opt/kafka_2.13-2.5.1/bin/kafka-server-start.sh /opt/kafka_2.13-2.5.1/config/server.properties \
-	--override log.dirs=/var/lib/kafka/data/topics \
-	--override controlled.shutdown.enable=true \
-	--override broker.id=${HOSTNAME##*-} \
-	--override listeners=INTERNAL://:${BROKER_SERVICE_PORT_INTERNAL},EXTERNAL://:${BROKER_SERVICE_PORT_INTERNAL_EXTERNAL} \
-	--override advertised.listeners=INTERNAL://${BROKER_SERVICE_HOST_INTERNAL}:${BROKER_SERVICE_PORT_INTERNAL},EXTERNAL://${BROKER_SERVICE_HOST_EXTERNAL}:${BROKER_SERVICE_PORT_EXTERNAL} \
-	--override listener.security.protocol.map=INTERNAL:PLAINTEXT,EXTERNAL:PLAINTEXT --override inter.broker.listener.name=INTERNAL \
-	--override zookeeper.connect=${ZOOKEEPER_SERVICE_HOST}:${ZOOKEEPER_SERVICE_PORT_CLIENT}
-#tail -f /dev/null
+# /opt/kafka_2.13-2.5.1/bin/kafka-server-start.sh /opt/kafka_2.13-2.5.1/config/server.properties \
+# 	--override log.dirs=/var/lib/kafka/data/topics \
+# 	--override controlled.shutdown.enable=true \
+# 	--override broker.id=${HOSTNAME##*-} \
+# 	--override listeners=INTERNAL://:${BROKER_SERVICE_PORT_INTERNAL},EXTERNAL://:${BROKER_SERVICE_PORT_INTERNAL_EXTERNAL} \
+# 	--override advertised.listeners=INTERNAL://${BROKER_SERVICE_HOST_INTERNAL}:${BROKER_SERVICE_PORT_INTERNAL},EXTERNAL://${BROKER_SERVICE_HOST_EXTERNAL}:${BROKER_SERVICE_PORT_EXTERNAL} \
+# 	--override listener.security.protocol.map=INTERNAL:PLAINTEXT,EXTERNAL:PLAINTEXT --override inter.broker.listener.name=INTERNAL \
+# 	--override zookeeper.connect=${ZOOKEEPER_SERVICE_HOST}:${ZOOKEEPER_SERVICE_PORT_CLIENT}
+tail -f /dev/null
